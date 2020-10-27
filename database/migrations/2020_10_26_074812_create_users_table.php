f64e2b090fcd3e975080380a107dfc6c3cbfcf80
vinhntph07621 <57892172+vinhntph07621@users.noreply.github.com>
2020-10-27 10:43:17 +0700

    Update 2020_10_26_074812_create_users_table.php

--- database/migrations/2020_10_26_074812_create_users_table.php
+++ database/migrations/2020_10_26_074812_create_users_table.php
@@ -22,6 +22,22 @@ class CreateUsersTable extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
+
+        Schema::create('role', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',50);
+            $table->integer('status');
+            $table->timestamps();
+        });
+
+        Schema::create('user_role', function (Blueprint $table) {
+            $table->id();
+            $table->index('user_id');
+            $table->index('role_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('restrict');
+            $table->foreign('role_id')->references('id')->on('role')->onDelete('restrict');
+            $table->timestamps();
+        });
     }
 
     /**
