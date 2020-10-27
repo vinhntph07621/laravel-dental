26f8daaf4aabfd9777109de4ffa37ce84e3498dc
vinhntph07621 <57892172+vinhntph07621@users.noreply.github.com>
2020-10-27 10:55:44 +0700

    a

--- database/migrations/2020_10_27_031137_create_role_table.php
+++ database/migrations/2020_10_27_031137_create_role_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateRoleTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('role', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('name',50);
+            $table->integer('status');
             $table->timestamps();
         });
-
     }
 
     /**
@@ -32,7 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('role');
     }
 }
