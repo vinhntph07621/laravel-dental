76fc5de7433e35b56408982fbf0bb9f055e4a039
vinhntph07621 <57892172+vinhntph07621@users.noreply.github.com>
2020-10-29 12:20:51 +0700

    Update RoleSeeder.php

--- database/seeds/RoleSeeder.php
+++ database/seeds/RoleSeeder.php
@@ -12,9 +12,13 @@ class RoleSeeder extends Seeder
     public function run()
     {
         //
-        DB::table('role')->insert([
-            'name' => 'Admin',
-            'status' => 1,
-        ]);
+        $role = [
+            ['id' => 1, 'name' => 'Admin', 'status' => 1],
+            ['id' => 2, 'name' => 'Doctor', 'status' => 1],
+            ['id' => 3, 'name' => 'Nurse', 'status' => 1],
+            ['id' => 4, 'name' => 'Customer', 'status' => 1],
+        ];
+
+        DB::table('role')->insert($role);
     }
 }
