0e6ae0450d92404f537556cfe5b03a20f7942e24
vinhntph07621 <57892172+vinhntph07621@users.noreply.github.com>
2020-10-29 11:39:31 +0700

    update seeder

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('users')->insert([
+            'name' => 'Vinh Nguyen',
+            'phone' => '0397361642',
+            'email' => 'vinhxx7@gmail.com',
+            'password' => Hash::make('password'),
+        ]);
+    }
+}
