d86d5643836d24596e59cc6dd790b27530c7360c
vinhntph07621 <57892172+vinhntph07621@users.noreply.github.com>
2020-10-29 23:34:04 +0700

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -50,7 +50,6 @@ class AuthController extends Controller
         $token->expries_at = Carbon::now()->addWeeks(1);
         $token->save();
         return response()->json([
-            'user' => $request->user(),
             'access_token' => $tokenResult->accessToken,
             'token_type' => 'Bearer',
             'expires_at' => Carbon::parse($tokenResult->token->expires_at)->toDateTimeString() 
