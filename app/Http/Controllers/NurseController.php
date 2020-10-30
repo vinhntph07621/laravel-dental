9070aaf103702567bb9f0e68db78a96942e3a1c0
vinhntph07621 <57892172+vinhntph07621@users.noreply.github.com>
2020-10-30 13:59:39 +0700

    Update NurseController.php

--- app/Http/Controllers/NurseController.php
+++ app/Http/Controllers/NurseController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use App\User;
-use App\Doctor;
+use App\Nurse;
 use App\UserRole;
 use Illuminate\Support\Facades\Auth;
 
