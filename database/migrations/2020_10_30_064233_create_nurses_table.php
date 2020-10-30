781db36b5251ff4071e8f1cf2eab6116981c6265
vinhntph07621 <57892172+vinhntph07621@users.noreply.github.com>
2020-10-30 13:44:05 +0700

    Create 2020_10_30_064233_create_nurses_table.php

---
+++ database/migrations/2020_10_30_064233_create_nurses_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNurseTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('nurses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name',255);
+            $table->string('last_name',255);
+            $table->date('birthday');
+            $table->integer('phone');
+            $table->string('address',500);
+            $table->string('email',255);
+            $table->string('avatar',255);
+            $table->boolean('gender');
+            $table->text('short_bio');
+            $table->integer('status');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('nurses');
+    }
+}
