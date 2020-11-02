625e842f8d91eeddcc5cdbca5ea6203426059285
vinhntph07621 <57892172+vinhntph07621@users.noreply.github.com>
2020-11-02 09:06:22 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -16,8 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login','AuthController@login');
 Route::post('/signup','UserController@signup');
 
-Route::get('/doctors','DoctorController@index');
-
 Route::get('/price-list', 'PriceListController@index');
 
 Route::get('/services', 'ServiceController@index');
