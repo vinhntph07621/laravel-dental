0cec08c9342aa5b4feea6aabb3ac53d8daf18bac
vinhntph07621 <57892172+vinhntph07621@users.noreply.github.com>
2020-11-02 16:16:56 +0700

    Create 2020_11_02_091411_create_detail_price_list_table.php

---
+++ database/migrations/2020_11_02_091411_create_detail_price_list_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDetailPriceListTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detail_price_list', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('price_list_id')->unsigned();
+            $table->string('unit');
+            $table->integer('status');
+            $table->foreign('price_list_id')->references('id')->on('price_list_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detail_price_list');
+    }
+}
