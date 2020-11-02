305f6467c0ef19c9a9a7e87adb257653d6b2ed83
vinhntph07621 <57892172+vinhntph07621@users.noreply.github.com>
2020-11-02 17:30:23 +0700

    Update detailPrice.php

--- app/detailPrice.php
+++ app/detailPrice.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class detailPrice extends Model
 {
     public $timestamps = false ;
-    protected $table ="detail_pricel_list";
+    protected $table ="detail_price_list";
     protected $fillable = [
          'price_list_id','unit','price','status','name'
     ];
