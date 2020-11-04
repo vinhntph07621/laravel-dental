1de8aa25234dedb3bc0607a8b388e9affa302f14
vinhntph07621 <57892172+vinhntph07621@users.noreply.github.com>
2020-11-04 13:07:18 +0700

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -19,6 +19,7 @@ class AuthController extends Controller
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
+            'phone' => $request->phone,
             'password' => bcrypt($request->password)
         ]);
         $user->save();
