02cf3a9b2b1fb8d58b58b7280da80d37ac253bb0
vinhntph07621 <57892172+vinhntph07621@users.noreply.github.com>
2020-11-09 10:20:47 +0700

    Update ContactController.php

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Contact;
 
 class ContactController extends Controller
 {
     //
+    public function index(){
+        $contacts = Contact::all();
+        return response()->json($contacts, 200);
+    }
+
+    public function store(Request $request){
+        $contact = Contact::create($request->all());
+        return response()->json($contacts, 200);
+    }
 }
