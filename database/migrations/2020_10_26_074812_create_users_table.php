f4fa3ac9fa86bc0e6db7d1c7632ee6b7aa4fb50b
vinhntph07621 <57892172+vinhntph07621@users.noreply.github.com>
2020-11-19 11:16:49 +0700

    Update 2020_10_26_074812_create_users_table.php

--- database/migrations/2020_10_26_074812_create_users_table.php
+++ database/migrations/2020_10_26_074812_create_users_table.php
@@ -20,8 +20,8 @@ class CreateUsersTable extends Migration
             $table->string('phone');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('avatar');
-            $table->integer('status');
+            $table->string('avatar')->nullable()->default('none.jpg');
+            $table->integer('status')->nullable()->default(1);
             $table->rememberToken();
             $table->timestamps();
         });
