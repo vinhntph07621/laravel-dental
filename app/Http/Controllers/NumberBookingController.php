ec8187a12fbfdf2cb66f821029b0d9f2abc1b6c3
vinhntph07621 <57892172+vinhntph07621@users.noreply.github.com>
2020-11-25 10:19:40 +0700

    Update NumberBookingController.php

--- app/Http/Controllers/NumberBookingController.php
+++ app/Http/Controllers/NumberBookingController.php
@@ -13,7 +13,7 @@ class NumberBookingController extends Controller
         $numberBookings = DB::table('number_booking')
         ->join('appointment','appointment.id','=','number_booking.appointment_id')
         ->join('doctors','doctors.id','=','appointment.doctor_id')
-        ->select('number_booking.*','appointment.patient_name','appointment.phone_number','appointment.date_time', DB::raw('concat(doctors.first_name," ",doctors.last_name) as doctor_name'))
+        ->select('number_booking.*','appointment.patient_name','appointment.phone_number','appointment.date_time', DB::raw("concat(doctors.first_name,' ',doctors.last_name) as doctor_name"))
         ->get();
         return response()->json($numberBookings, 200);
     }
