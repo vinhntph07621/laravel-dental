eb254fa932e65445a384b0800903a0051551fc31
vinhntph07621 <57892172+vinhntph07621@users.noreply.github.com>
2020-11-27 16:39:40 +0700

    Update MedicalRecordController.php

--- app/Http/Controllers/MedicalRecordController.php
+++ app/Http/Controllers/MedicalRecordController.php
@@ -13,8 +13,9 @@ class MedicalRecordController extends Controller
     public function index(){
         $medicalRecords = NumberBooking::with('re_examination')
         ->join('medical_record','medical_record.number_booking_id','=','number_booking_id')
-        ->join('number_booking','number_booking.id','=','medical_record.number_booking_id')
-        ->select('medical_record.*')
+        ->join('appointment','appointment.id','=','number_booking.appointment_id')
+        ->join('doctors','doctors.id','=','appointment.doctor_id')
+        ->select('medical_record.*','appointment.patient_name','appointment.phone_number', DB::raw("concat(doctors.first_name,' ',doctors.last_name) as doctor_name"))
         ->get();
         return response()->json($medicalRecords, 200);
     }
