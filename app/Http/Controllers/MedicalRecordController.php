c9a3c846d4b841c2c0af3c1f2a0e1e387314ddf5
vinhntph07621 <57892172+vinhntph07621@users.noreply.github.com>
2020-11-27 16:33:51 +0700

    aaaa

--- app/Http/Controllers/MedicalRecordController.php
+++ app/Http/Controllers/MedicalRecordController.php
@@ -13,6 +13,7 @@ class MedicalRecordController extends Controller
     public function index(){
         $medicalRecords = NumberBooking::with('re_examination')
         ->join('medical_record','medical_record.number_booking_id','=','number_booking_id')
+        ->join('number_booking','number_booking.id','=','medical_record.number_booking_id')
         ->select('medical_record.*')
         ->get();
         return response()->json($medicalRecords, 200);
--- app/Http/Controllers/ReExaminationController.php
+++ app/Http/Controllers/ReExaminationController.php
@@ -41,6 +41,22 @@ class ReExaminationController extends Controller
         return response()->json($reExaminationByUser, 200);
     }
 
+    public function getDetail($id){
+        $users = Auth::user();
+        $user_id = $users->id;
+        DB::enableQueryLog();
+
+        $reExaminationByUser = DB::table('re_examination')
+        ->join('number_booking','number_booking.id','=','re_examination.number_booking_id')
+        ->join('appointment','appointment.id','=','number_booking.appointment_id')
+        ->join('doctors','doctors.id','=','appointment.doctor_id')
+        ->select('re_examination.*','appointment.patient_name', 'appointment.phone_number', DB::raw("concat(doctors.first_name,' ',doctors.last_name) as doctor_name"))
+        ->where('re_examination.id','=',$id)
+        ->where('appointment.user_id','=',$user_id)
+        ->get();
+        return response()->json($reExaminationByUser, 200);
+    }
+
     public function store(Request $request){
         $checkStatus = DB::table('re_examination')
         ->where('number_booking_id', '=', $request->number_booking_id)
