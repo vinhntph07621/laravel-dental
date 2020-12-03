6953e66e7f8856c15deba7502ed3adf12d7a4311
vinhntph07621 <57892172+vinhntph07621@users.noreply.github.com>
2020-12-03 11:43:33 +0700

    Update MedicalRecordController.php

--- app/Http/Controllers/MedicalRecordController.php
+++ app/Http/Controllers/MedicalRecordController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\NumberBooking;
+use Illuminate\Support\Facades\Auth;
 use App\MedicalRecord;
 
 class MedicalRecordController extends Controller
