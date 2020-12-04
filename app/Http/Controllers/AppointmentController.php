e6d2c23ba7aaaf68cf48b1f425f53451a671faaa
vinhntph07621 <57892172+vinhntph07621@users.noreply.github.com>
2020-12-04 14:05:44 +0700

    Update AppointmentController.php

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -43,7 +43,8 @@ class AppointmentController extends Controller
         
         $appointments = DB::table('appointment')
         ->join('doctors','doctors.id','=','appointment.doctor_id')
-        ->select('appointment.id','doctors.first_name as doctor_first_name','doctors.last_name as doctor_last_name','appointment.status','appointment.has_people','appointment.date_time','appointment.patient_name')
+        ->leftJoin('number_booking','number_booking.appointment_id','=','appointment.id')
+        ->select('number_booking.id as number_booking_id','appointment.id','doctors.first_name as doctor_first_name','doctors.last_name as doctor_last_name','appointment.status','appointment.has_people','appointment.date_time','appointment.patient_name')
         ->orderBy('appointment.id', 'DESC')
         ->where('appointment.user_id',$user_id)
         ->where('appointment.status','!=','3')
