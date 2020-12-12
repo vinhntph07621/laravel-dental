35677535a9ccfd9eaac827784fe413aa4a899776
vinhntph07621 <57892172+vinhntph07621@users.noreply.github.com>
2020-12-12 14:33:36 +0700

    Update DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -24,9 +24,11 @@ class DashboardController extends Controller
             'countDoctor' => $doctors,
             'countUser' => $users,
             'countNumberBooking' => $numberBookings,
-            'countMedicalRecord' => $MedicalRecords,
+            'countMedicalRecord' => $medicalRecords,
         ], 200);
     }
+
+    // Doctor
 
     public function getBookingCurrentByDoctor(){
         $users = Auth::user();
@@ -53,6 +55,34 @@ class DashboardController extends Controller
         ->get();
         return response()->json($getListBookingToDay, 200);
     }
+
+    public function getBookingCurrentByDoctorComplete(){
+        $users = Auth::user();
+        $user_id = $users->id;
+
+        $checkLogin = DB::table('doctors')
+        ->join('users','users.id','=','doctors.user_id')
+        ->select('doctors.id as doctor_id')
+        ->where('users.id','=',$user_id)
+        ->get();
+
+        $startDate = CarBon::now()->toDateString();
+        $endDate = CarBon::now()->addDay()->toDateString();
+
+        $getListBookingToDay = DB::table('number_bookings')
+        ->join('appointments','appointments.id','=','number_bookings.appointment_id')
+        ->join('doctors','doctors.id','=','appointments.doctor_id')
+        ->select('number_bookings.*','appointments.patient_name','appointments.phone_number','appointments.date_time', DB::raw("concat(doctors.first_name,' ',doctors.last_name) as doctor_name"))
+        ->where('number_bookings.status','=',2)
+        ->where('doctors.id','=',$checkLogin[0]->doctor_id)
+        ->where('appointments.date_time','>=',$startDate)
+        ->where('appointments.date_time','<',$endDate)
+        ->orderBy('number_bookings.id','DESC')
+        ->get();
+        return response()->json($getListBookingToDay, 200);
+    }
+
+    // Admin
 
     public function getBookingCurrentByAdmin(){
         $users = Auth::user();
@@ -92,29 +122,5 @@ class DashboardController extends Controller
         return response()->json($getListBookingToDay, 200);
     }
 
-    public function getBookingCurrentByDoctorComplete(){
-        $users = Auth::user();
-        $user_id = $users->id;
-
-        $checkLogin = DB::table('doctors')
-        ->join('users','users.id','=','doctors.user_id')
-        ->select('doctors.id as doctor_id')
-        ->where('users.id','=',$user_id)
-        ->get();
-
-        $startDate = CarBon::now()->toDateString();
-        $endDate = CarBon::now()->addDay()->toDateString();
-
-        $getListBookingToDay = DB::table('number_bookings')
-        ->join('appointments','appointments.id','=','number_bookings.appointment_id')
-        ->join('doctors','doctors.id','=','appointments.doctor_id')
-        ->select('number_bookings.*','appointments.patient_name','appointments.phone_number','appointments.date_time', DB::raw("concat(doctors.first_name,' ',doctors.last_name) as doctor_name"))
-        ->where('number_bookings.status','=',2)
-        ->where('doctors.id','=',$checkLogin[0]->doctor_id)
-        ->where('appointments.date_time','>=',$startDate)
-        ->where('appointments.date_time','<',$endDate)
-        ->orderBy('number_bookings.id','DESC')
-        ->get();
-        return response()->json($getListBookingToDay, 200);
-    }
+    
 }
