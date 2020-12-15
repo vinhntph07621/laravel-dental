775ee103293140ddd91ee60d21a6a32f5ce396b3
vinhntph07621 <57892172+vinhntph07621@users.noreply.github.com>
2020-12-15 17:10:35 +0700

    Update ResetPasswordRequest.php

--- app/Notifications/ResetPasswordRequest.php
+++ app/Notifications/ResetPasswordRequest.php
@@ -38,8 +38,8 @@ class ResetPasswordRequest extends Notification implements ShouldQueue
         $url = url('reset-password/?token=' . $this->token);
         
         return (new MailMessage)
-            ->line('You are receiving this email because we received a password reset request for your account.')
-            ->action('Reset Password', url($url))
-            ->line('If you did not request a password reset, no further action is required.');
+            ->line('Bạn nhận được email này vì chúng tôi đã nhận được yêu cầu đặt lại mật khẩu cho tài khoản của bạn.')
+            ->action('Đặt lại mật khẩu', url($url))
+            ->line('Nếu bạn không yêu cầu đặt lại mật khẩu, bạn không cần thực hiện thêm hành động nào');
     }
 }
